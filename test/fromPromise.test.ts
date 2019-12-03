9c4e0e7e05faf389cee8d24bc9d22087b63c1446
Alexis Munsayac <alexis@lyon.com.ph>
2019-12-04 00:14:20 +0800

    Create fromPromise.test.ts

---
+++ test/fromPromise.test.ts
@@ -0,0 +1,15 @@
+import { fromPromise } from '../src';
+
+describe('fromPromise', () => {
+  it('should resolve with the same resolved value', () => {
+    const base = 'hello';
+
+    expect(fromPromise(Promise.resolve(base)).get()).resolves.toEqual(base);
+  });
+
+  it('should reject with the same rejected value', async () => {
+    const base = 'hello';
+
+    expect(fromPromise(Promise.reject(base)).get()).rejects.toEqual(base);
+  });
+});
