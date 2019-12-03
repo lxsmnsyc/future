76c8198e7634ad495a0dacb9143eaf9bc3c034a0
Alexis Munsayac <alexis@lyon.com.ph>
2019-12-04 00:09:50 +0800

    Create computation.ts

---
+++ src/computation.ts
@@ -0,0 +1,26 @@
+import Subscription from './subscription';
+import { Function, Action } from './utils/types/function';
+
+type Resolve<T, R> = Function<T, R | PromiseLike<R>> | null | undefined;
+type Reject<R> = Function<any, R | PromiseLike<R>> | null | undefined;
+type Finally = Action | null | undefined;
+
+export default class Computation<T> implements Subscription {
+  constructor(private result: Promise<T>, private subscription: Subscription) {}
+
+  public cancel() {
+    this.subscription.cancel();
+  }
+
+  public then<R>(resolve?: Resolve<T, R>, reject?: Reject<R>) {
+    return this.result.then(resolve, reject);
+  }
+
+  public catch<R>(reject?: Reject<R>) {
+    return this.result.catch(reject);
+  }
+
+  public finally(action?: Finally) {
+    return this.result.finally(action);
+  }
+}
