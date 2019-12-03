e1533992179860ab2d9b79e936049956758c9af5
Alexis Munsayac <alexis@lyon.com.ph>
2019-12-04 00:12:29 +0800

    Create merge.ts

---
+++ src/operators/merge.ts
@@ -0,0 +1,73 @@
+/**
+ * @license
+ * MIT License
+ *
+ * Copyright (c) 2019 Alexis Munsayac
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+ *
+ * @author Alexis Munsayac <alexis.munsayac@gmail.com>
+ * @copyright Alexis Munsayac 2019
+ */
+import Future from '../future';
+import Computation from '../computation';
+import CompositeSubscription from '../utils/subscriptions/composite-subscription';
+
+
+class FutureMerge<T> extends Future<T> {
+  constructor(
+    private future: Future<Future<T>>,
+  ) {
+    super();
+  }
+
+  get(): Computation<T> {
+    const subscription = new CompositeSubscription();
+
+    const promise = new Promise<T>((resolve, reject) => {
+      const res = (value: T) => !subscription.cancelled && resolve(value);
+      const rej = (value: Error) => !subscription.cancelled && reject(value);
+
+      const computation = this.future.get();
+
+      computation.then(
+        value => {
+          if (!subscription.cancelled) {
+            const newComputation = value.get();
+
+            subscription.add(newComputation);
+            
+            newComputation.then(res, rej);
+          }
+        },
+        rej,
+      );
+    });
+
+    return new Computation<T>(promise, subscription);
+  }
+}
+
+/**
+ * Transforms the resolved value of the given Future.
+ * @param mapper a function that transforms the resolved value
+ */
+export default function merge<T>(future: Future<Future<T>>): Future<T> {
+  return new FutureMerge<T>(future);
+}
